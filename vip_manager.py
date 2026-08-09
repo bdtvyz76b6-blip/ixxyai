e547ffdf7f335ad962561c6c9e0c6c772a206949
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-08-09 09:33:24 +0300

    Update vip_manager.py

--- vip_manager.py
+++ vip_manager.py
@@ -1,10 +1,8 @@
 import json
 import os
 from threading import Lock
-from config import FREE_LIMIT
 
 VIP_FILE = "vips.json"
-FREE_FILE = "free_usage.json"
 lock = Lock()
 
 def load_vips():
@@ -25,26 +23,3 @@ def add_vip(user_id: int):
         vips = load_vips()
         vips.add(user_id)
         save_vips(vips)
-
-def load_free_usage():
-    if not os.path.exists(FREE_FILE):
-        return {}
-    with open(FREE_FILE) as f:
-        return json.load(f)
-
-def save_free_usage(data):
-    with open(FREE_FILE, "w") as f:
-        json.dump(data, f)
-
-def can_use_free(user_id: int) -> bool:
-    with lock:
-        data = load_free_usage()
-        used = data.get(str(user_id), 0)
-        return used < FREE_LIMIT
-
-def use_free(user_id: int):
-    with lock:
-        data = load_free_usage()
-        uid = str(user_id)
-        data[uid] = data.get(uid, 0) + 1
-        save_free_usage(data)
